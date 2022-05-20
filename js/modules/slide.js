7d664d03c956ab656acd66022ea75ca236927ffb
Brain <toolsbrain9@gmail.com>
2022-05-20 11:53:35 -0300

    #

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -8,6 +8,9 @@ export default function createSlide() {
   const slideOne = document.querySelector("#carousel-one");
   const slideTwo = document.querySelector("#carousel-two");
   const slideThree = document.querySelector("#carousel-three");
+  const slideFour = document.querySelector(".reuniao_slide");
+  const slideFive = document.querySelector(".reuniao_ups8_slide");
+  const slideSix = document.querySelector(".reuniao_ups8_slide_02");
 
   function showSlide() {
     const options = {
@@ -55,6 +58,31 @@ export default function createSlide() {
       },
     };
 
+    const optionsThree = {
+      type: "false",
+      autoplay: true,
+      perMove: 1,
+      speed: 300,
+      interval: 2500,
+      rewind: true,
+      perPage: 2,
+      gap: 30,
+      pagination: false,
+      lazyLoad: "nearby",
+      breakpoints: {
+        760: {
+          perPage: 2,
+          gap: 30,
+          lazyLoad: "nearby",
+        },
+        580: {
+          perPage: 1,
+          gap: 30,
+          lazyLoad: "nearby",
+        },
+      },
+    };
+
     if (slideOne) {
       new Splide("#carousel-one", options).mount({ Video });
     }
@@ -63,6 +91,15 @@ export default function createSlide() {
     }
     if (slideThree) {
       new Splide("#carousel-three", optionsTwo).mount();
+    }
+    if (slideFour) {
+      new Splide(".reuniao_slide", options).mount();
+    }
+    if (slideFive) {
+      new Splide(".reuniao_ups8_slide", optionsThree).mount();
+    }
+    if (slideSix) {
+      new Splide(".reuniao_ups8_slide_02", optionsThree).mount();
     }
   }
 
